b95d59e4894f40f193c06a2c89f2fa38358bd340
readloud <forever8124@gmail.com>
2026-06-06 15:19:05 +0700

    release

--- js/purchase.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function () {
-    var $buythemediv = '<div class="buy-theme alt-font xs-display-none"><a href="https://discord.gg/readloud" target="_blank"><span>Join Our Server</span></a></div><div class="quick-question alt-font xs-display-none"><a href="https://guns.lol/readloud"><span>Our Community</span></a></div>';
-    $('body').append($buythemediv);
-});
